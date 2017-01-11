73fb28e8ff7e0845f1ed270b3bed43cb672a4063
Will Chertoff <willchertoff@gmail.com>
2017-01-11 16:36:00 +0100

    feat(production): Modify API_ADDRESS

--- src/constants/Config.js
+++ src/constants/Config.js
@@ -1,6 +1,6 @@
 export const CLIENT_ID = "57f5390a5fdec70078e5923c";
 export const GOOGLE_MAPS_API = 'AIzaSyC_ITcc0I_2wC21ct9y-He2BNAVRP57JsE';
-export const API_ADDRESS = 'http://ec2-35-162-31-26.us-west-2.compute.amazonaws.com:3000';
+export const API_ADDRESS = 'http://ec2-35-165-128-137.us-west-2.compute.amazonaws.com:8080';
 export const API_USER_ID = '5824f681633ba9592252c74c';
 export const API_USERNAME = 'nea';
 export const API_PASSWORD = 'nea-hcpd';
